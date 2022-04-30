8fc9e5f86b9183dea92063147fbee3d51148801c
Gavin Eubank <72398224+eubankgs@users.noreply.github.com>
2022-04-30 15:04:52 -0400

    aproximation tests

---
+++ runaproxx.sh
@@ -0,0 +1,10 @@
+python3 maxclique_approx.py < testcases/input1.txt > testcases/aprox_output1.txt
+python3 maxclique_approx.py < testcases/input2.txt > testcases/aprox_output2.txt
+python3 maxclique_approx.py < testcases/input3.txt > testcases/aprox_output3.txt
+python3 maxclique_approx.py < testcases/input4.txt > testcases/aprox_output4.txt
+python3 maxclique_approx.py < testcases/input5.txt > testcases/aprox_output5.txt
+python3 maxclique_approx.py < testcases/input6.txt > testcases/aprox_output6.txt
+python3 maxclique_approx.py < testcases/input7.txt > testcases/aprox_output7.txt
+python3 maxclique_approx.py < testcases/input7.txt > testcases/aprox_output7.txt
+python3 maxclique_approx.py < testcases/input8.txt > testcases/aprox_output8.txt
+python3 maxclique_approx.py < testcases/input9.txt > testcases/aprox_output9.txt

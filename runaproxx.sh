8b2d3675dc364098da8d56848935e33e9ffbff9e
Gavin Eubank <72398224+eubankgs@users.noreply.github.com>
2022-04-30 16:17:05 -0400

    aproximation script

--- runaproxx.sh
+++ runaproxx.sh
@@ -1,10 +1,20 @@
-python3 maxclique_approx.py < testcases/input1.txt > testcases/aprox_output1.txt
-python3 maxclique_approx.py < testcases/input2.txt > testcases/aprox_output2.txt
-python3 maxclique_approx.py < testcases/input3.txt > testcases/aprox_output3.txt
-python3 maxclique_approx.py < testcases/input4.txt > testcases/aprox_output4.txt
-python3 maxclique_approx.py < testcases/input5.txt > testcases/aprox_output5.txt
-python3 maxclique_approx.py < testcases/input6.txt > testcases/aprox_output6.txt
-python3 maxclique_approx.py < testcases/input7.txt > testcases/aprox_output7.txt
-python3 maxclique_approx.py < testcases/input7.txt > testcases/aprox_output7.txt
-python3 maxclique_approx.py < testcases/input8.txt > testcases/aprox_output8.txt
-python3 maxclique_approx.py < testcases/input9.txt > testcases/aprox_output9.txt
+kernprof -l maxclique_approx.py < testcases/input1.txt > testcases/aprox_output1.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_1_profile.txt
+kernprof -l maxclique_approx.py < testcases/input2.txt > testcases/aprox_output2.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_2_profile.txt
+kernprof -l maxclique_approx.py < testcases/input3.txt > testcases/aprox_output3.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_3_profile.txt
+kernprof -l maxclique_approx.py < testcases/input4.txt > testcases/aprox_output4.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_4_profile.txt
+kernprof -l maxclique_approx.py < testcases/input5.txt > testcases/aprox_output5.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_5_profile.txt
+kernprof -l maxclique_approx.py < testcases/input6.txt > testcases/aprox_output6.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_6_profile.txt
+kernprof -l maxclique_approx.py < testcases/input7.txt > testcases/aprox_output7.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_7_profile.txt
+kernprof -l maxclique_approx.py < testcases/input8.txt > testcases/aprox_output8.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_8_profile.txt
+kernprof -l maxclique_approx.py < testcases/input9.txt > testcases/aprox_output9.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_9_profile.txt
+kernprof -l maxclique_approx.py < testcases/input10.txt > testcases/aprox_output10.txt
+python3 -m line_profiler maxclique_approx.py.lprof > maxclique_approx_10_profile.txt
